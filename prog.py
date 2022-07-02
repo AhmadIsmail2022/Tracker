c94633195e97a49aa940747d6fda321d8311ed57
Ahmad Ismail <104082071+AhmadIsmail2022@users.noreply.github.com>
2022-07-02 22:08:29 +0300

    Update prog.py

--- prog.py
+++ prog.py
@@ -1,15 +1,12 @@
 #!/usr/bin/python
 # -*- coding: latin-1 -*-
 #--------------------------------------------------------------------------------
-#  Company : Горизонт 
-#  Project : Measuring disk center
 #  Program : Application for microcontroller system to implmenet 
 #            Algorithm for estimation center corrdinates in Sub-Pixel Accuracy.
 #
 #  Created : 08.05.2022
 #  Version : 1.3.1
 #  Date    : 06.06.2022
-#  Developing History: file History.txt
 
 #-------------------------------------------------------------------------------
 
